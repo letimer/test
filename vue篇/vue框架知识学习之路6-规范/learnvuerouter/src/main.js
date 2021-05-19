273b2af637f4484164b7cbf6d1d18ba1e2190eb2
letimer <2374125433@qq.com>
2021-05-19 23:45:56 +0800

    路由的简单使用

--- vue篇/vue框架知识学习之路6-规范/learnvuerouter/src/main.js
+++ vue篇/vue框架知识学习之路6-规范/learnvuerouter/src/main.js
@@ -7,6 +7,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  router:router,
   render: h => h(App)
 })
--- vue篇/vue框架知识学习之路6-规范/learnvuerouter/src/router/index.js
+++ vue篇/vue框架知识学习之路6-规范/learnvuerouter/src/router/index.js
@@ -1,15 +1,50 @@
+// 配置路由相关的信息
+import VueRouter from 'vue-router'
+// 通过Vue.use（插件）,安装插件，因为没有Vue,所以导入import Vue from 'vue'
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router)
+import Home from "../components/Home"
+import About from "../components/About"
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+// 1.安装插件
+Vue.use(VueRouter)
+
+//将routes单拎出来，方便编写和阅读
+const routes=[
+  {
+    path:'/home',
+    component:Home
+  },
+  {
+    path:'/about',
+    component:About
+  }
+]
+// 2.创建VueRouter对象
+const router=new VueRouter({
+  // 配置路径和组件之间的映射关系
+  routes
 })
+
+// 3.将router对象传入到Vue实例,将其导出
+export default router
+
+
+
+//该文件中原始的代码如下
+// import Vue from 'vue'
+// import VueRouter from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+// //安装插件
+// Vue.use(VueRouter);
+// export default new VueRouter({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'HelloWorld',
+//       component: HelloWorld
+//     }
+//   ]
+// })
+
+
