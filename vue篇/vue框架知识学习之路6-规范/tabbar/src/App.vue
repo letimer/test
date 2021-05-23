3c1565faf65f8b0e7ea55e1dabcb45209a308cf6
letimer <2374125433@qq.com>
2021-05-23 12:04:10 +0800

    tabbar的基本结构的搭建

---
+++ vue篇/vue框架知识学习之路6-规范/tabbar/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div id="tab-bar">
+      <div>首页</div>
+      <div>分类</div>
+      <div>购物车</div>
+      <div>我的</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {  }
+}
+</script>
+
+<style>
+</style>
